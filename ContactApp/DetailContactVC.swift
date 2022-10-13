facf565aa3dfa864b956f1714ecfb83d04d125c6
Aytaç Bulanık <aytacbulanik@hotmail.com>
2022-10-13 15:23:04 +0300

    images and plist added

--- ContactApp/DetailContactVC.swift
+++ ContactApp/DetailContactVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  DetailContactVC.swift
 //  ContactApp
 //
 //  Created by AYTAÇ BULANIK on 13.10.2022.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class DetailContactVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
 
-
 }
-
