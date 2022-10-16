dc764556e7cbc6d2751c1caec53bbef0e9279a42
Aytaç Bulanık <aytacbulanik@hotmail.com>
2022-10-16 16:34:23 +0300

    detail cell added all data

--- ContactApp/Controller/DetailVC.swift
+++ ContactApp/Controller/DetailVC.swift
@@ -8,16 +8,32 @@
 import UIKit
 
 class DetailVC: UITableViewController {
-        @IBOutlet var personImage : UIImageView!
+    @IBOutlet var personImage : UIImageView!
+    @IBOutlet var personNameLabel : UILabel!
+    @IBOutlet var phoneNumberLabel : UILabel!
+    @IBOutlet var mailLabel : UILabel!
+    @IBOutlet var addressLabel : UILabel!
+    
         var contact : Contact?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        guard let contact else {return}
-        personImage.image = UIImage(named: contact.avatarName)
+        detailContact(object: contact)
+        
     }
 
-  
+    func detailContact(object contact : Contact?) {
+        guard let contact else {return}
+        personImage.image = UIImage(named: contact.avatarName)
+        personNameLabel.text = "\(contact.firstName) \(contact.lastName.uppercased())"
+        phoneNumberLabel.text = contact.phoneNumber
+        mailLabel.text = contact.email
+        addressLabel.text = """
+            \(contact.streetAddress)
+            \(contact.zip)
+            \(contact.city) / \(contact.state)
+        """
+    }
 
    
 }
