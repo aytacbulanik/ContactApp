385a803125185e886ddddcad94fcbc87830d7871
Aytaç Bulanık <aytacbulanik@hotmail.com>
2022-10-16 12:51:38 +0300

    static cell and data are working

--- ContactApp/Controller/DetailVC.swift
+++ ContactApp/Controller/DetailVC.swift
@@ -8,33 +8,16 @@
 import UIKit
 
 class DetailVC: UITableViewController {
+        @IBOutlet var personImage : UIImageView!
+        var contact : Contact?
     
-        var contact : Contact? {
-            didSet {
-                print(contact?.firstName)
-            }
-        }
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        guard let contact else {return}
+        personImage.image = UIImage(named: contact.avatarName)
     }
 
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
+  
 
    
 }
